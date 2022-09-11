d6cfc35644229ea983ee0a08bc89efd19662b706
Andrew-McCall <amccall@qa.com>
2022-09-11 21:28:36 +0100

    Created Project API Controller (GetAll)

---
+++ andrew-mccall/Controllers/ProjectAPIController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using andrew_mccall.DAO;
+using andrew_mccall.Entites;
+
+namespace andrew_mccall.Controllers
+{
+    public class ProjectAPIController : Controller
+    {
+        private readonly ILogger<ProjectAPIController> _logger;
+        private ProjectDAO projectDAO = new ProjectDAO();
+
+        public ProjectAPIController(ILogger<ProjectAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Projects/GetAll")]
+        public IActionResult GetAll(){
+            projectDAO.Create(new Project("Title", "Description", "Link", "Image", false));
+            projectDAO.GetAll();
+            return Ok("lol");
+        }
+
+        public IActionResult Create(){
+            
+        }
+
+    }
+}
