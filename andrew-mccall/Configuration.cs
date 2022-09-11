a1c120fd877fd0cf19e1909d13abf84f2330ae3d
Andrew-McCall <amccall@qa.com>
2022-09-11 22:03:05 +0100

    Setup App.config and global variables

---
+++ andrew-mccall/Configuration.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace andrew_mccall
+{
+    static class GlobalConfiguration
+    {
+        public static String API_MASTER_KEY = "Example";
+    }
+}
--- andrew-mccall/Program.cs
+++ andrew-mccall/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,11 +11,25 @@ using Microsoft.Extensions.Logging;
 
 namespace andrew_mccall
 {
+
     public class Program
     {
+
         public static void Main(string[] args)
         {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["API_MASTER_KEY"])){
+                throw new Exception("Missing App.config, Should have Key API_MASTER_KEY");
+            }
+            
+            if (ConfigurationManager.AppSettings["API_MASTER_KEY"] == "Example"){
+                Console.Error.WriteLine("WARNING:");
+                WarningException warning = new WarningException("Default API_MASTER_KEY, Go to App.config to change");
+                Console.Error.WriteLine(warning);
+                Console.Error.WriteLine();
+            }
+            
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,4 +39,5 @@ namespace andrew_mccall
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
 }
